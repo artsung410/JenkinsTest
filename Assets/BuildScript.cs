6ea8b55941e478d0d2c9084704a4b40c3600b817
artsung <61309735+artsung410@users.noreply.github.com>
2026-02-02 21:28:55 +0900

    2

---
+++ Assets/BuildScript.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using UnityEditor;
+
+public class BuildScript
+{
+    public static void BuildWindows()
+    {
+        var senes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+
+        BuildPipeline.BuildPlayer(
+            senes,
+            "Build/Windows/MyGame.exe",
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None
+        );
+    }
+}
