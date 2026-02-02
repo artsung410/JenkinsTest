1d7bf249643e23dc4f11deb422ed8564053998d8
artsung <61309735+artsung410@users.noreply.github.com>
2026-02-02 22:03:11 +0900

    4

--- Assets/BuildScript.cs
+++ Assets/BuildScript.cs
@@ -1,17 +1,26 @@
+using System;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 
 public class BuildScript
 {
     public static void BuildWindows()
     {
-        var senes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+        var version = Environment.GetEnvironmentVariable("BUILD_VERSION");
+        if (!string.IsNullOrEmpty(version))
+            PlayerSettings.bundleVersion = version;
 
-        BuildPipeline.BuildPlayer(
-            senes,
-            "Build/Windows/Jenkins.exe",
-            BuildTarget.StandaloneWindows64,
-            BuildOptions.None
-        );
+        var scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+
+        var report = BuildPipeline.BuildPlayer(
+                    scenes,
+                    $"Build/Windows/Jenkins_{PlayerSettings.bundleVersion}.exe",
+                    BuildTarget.StandaloneWindows64,
+                    BuildOptions.None
+                );
+
+        if (report.summary.result != BuildResult.Succeeded)
+            throw new Exception("Build Failed: " + report.summary.result);
     }
 }
